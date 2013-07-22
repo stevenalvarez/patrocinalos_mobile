30dcdeb287c4eb1654a45143ce06313428653a19
jhonny <steven.alvarez.v@gmail.com>
2013-07-22 15:25:09 -0400

    config.js para add la base_url y no pisarnos los cambios

--- www/js/main.js
+++ www/js/main.js
@@ -1,15 +1,3 @@
-/************************************ VARIABLES DE CONFIGURACION *******************************************************/
-
-/************************************ server *******************************************************/
-var serviceURL = "http://patrocinalos.com/services/";
-var BASE_URL = "http://patrocinalos.com/";
-var BASE_URL_APP = "http://patrocinalos.com/";
-
-/************************************ localhost *******************************************************/
-//var serviceURL = "http://localhost/MOBILE_PATROCINALOS/patrocinalos_mobile/services/";
-//var BASE_URL = "http://localhost/MOBILE_PATROCINALOS/patrocinalos_mobile/";
-//var BASE_URL_APP="http://localhost/BITBUCKET_RED_SOCIAL/aplicacion/";
-
 /************************************ BIND EVENT *******************************************************/
 
 $(document).bind('pageinit', function(){
@@ -168,4 +156,3 @@ function getElites(){
     
     
 }
-
