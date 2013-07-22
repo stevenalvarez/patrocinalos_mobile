05d85098a4dc1007bf010bd529a7f522005bb730
jhonny <steven.alvarez.v@gmail.com>
2013-07-22 15:52:19 -0400

    otra vez

---
+++ www/js/config.js
@@ -0,0 +1,11 @@
+/************************************ VARIABLES DE CONFIGURACION *******************************************************/
+
+/************************************ server *******************************************************/
+var serviceURL = "http://patrocinalos.com/services/";
+var BASE_URL = "http://patrocinalos.com/";
+var BASE_URL_APP = "http://patrocinalos.com/";
+
+/************************************ localhost *******************************************************/
+//var serviceURL = "http://localhost/MOBILE_PATROCINALOS/patrocinalos_mobile/services/";
+//var BASE_URL = "http://localhost/MOBILE_PATROCINALOS/patrocinalos_mobile/";
+//var BASE_URL_APP="http://localhost/BITBUCKET_RED_SOCIAL/aplicacion/";
