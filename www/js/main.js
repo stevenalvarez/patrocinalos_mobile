a68b4f53e28e7278aee54c9616791fbb42acde89
jhonny <steven.alvarez.v@gmail.com>
2013-07-22 14:54:43 -0400

    base url

--- www/js/main.js
+++ www/js/main.js
@@ -5,9 +5,9 @@ var serviceURL = "http://patrocinalos.com/services/";
 var BASE_URL = "http://patrocinalos.com/";
 
 /************************************ localhost *******************************************************/
-var serviceURL = "http://localhost/MOBILE_PATROCINALOS/patrocinalos_mobile/services/";
-var BASE_URL = "http://localhost/MOBILE_PATROCINALOS/patrocinalos_mobile/";
-var BASE_URL_APP="http://localhost/BITBUCKET_RED_SOCIAL/aplicacion/";
+//var serviceURL = "http://localhost/MOBILE_PATROCINALOS/patrocinalos_mobile/services/";
+//var BASE_URL = "http://localhost/MOBILE_PATROCINALOS/patrocinalos_mobile/";
+//var BASE_URL_APP="http://localhost/BITBUCKET_RED_SOCIAL/aplicacion/";
 
 /************************************ BIND EVENT *******************************************************/
 
