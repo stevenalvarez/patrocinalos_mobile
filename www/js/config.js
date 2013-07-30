6117c3a14a0b38ca24e206d12625f9859407521c
jhonny <steven.alvarez.v@gmail.com>
2013-07-30 15:26:19 -0400

    refactorizacion, solo manejamos la BASE_URL_APP lo demas chingacion

--- www/js/config.js
+++ www/js/config.js
@@ -1,11 +1,7 @@
 /************************************ VARIABLES DE CONFIGURACION *******************************************************/
 
 /************************************ server *******************************************************/
-var serviceURL = "http://patrocinalos.com/services/";
-var BASE_URL = "http://patrocinalos.com/";
 var BASE_URL_APP = "http://www.patrocinalos.com/app/webroot/redsocialtest/";
 
 /************************************ localhost *******************************************************/
-//var serviceURL = "asdads";
-//var BASE_URL = "";
 //var BASE_URL_APP = "";
