ce29993b348be0b7b11cf5aa3c32973edf1f189b
jhonny <steven.alvarez.v@gmail.com>
2013-10-17 21:19:46 -0400

    no es lo correcto pero asi funca el carrousel, arreglar bien en otra

--- www/js/main.js
+++ www/js/main.js
@@ -157,7 +157,7 @@ $(document).on('pageinit', "#login", function(){
 });
 
 //HOME
-$('#home').live('pagebeforeshow', function(event, ui) {
+$(document).on('pageinit', "#home", function(){    
     if(isLogin()){
         var hash = window.location.hash;
         getEntradasByCarrousel($(this).attr('id'), hash);
@@ -469,16 +469,15 @@ function getEntradasByCarrousel(parent_id, hash){
                 
                 //iniciamos el carousel
                 parent.find("#carrousel_proyectos").find(".m-carousel-inner").promise().done(function() {
-                    //ocultamos loading
-                    $.mobile.loading( 'hide' );
-                    parent.find(".ui-content").fadeIn("slow");
-                    
                     //volvemos a recargar el plugin unicamente si es que los datos son mas de 1, porque inicialmente el html es uno y eso cuenta
                     if(items_proyectos > 1){
                         parent.find('#carrousel_proyectos').carousel("reload");
                     }else{
-                        parent.find('#carrousel_proyectos').carousel("reload");
+                        parent.find('#carrousel_proyectos').carousel();
                     }
+                    //ocultamos loading
+                    $.mobile.loading( 'hide' );
+                    parent.find(".ui-content").fadeIn("slow");
                 });
             }
         } 
