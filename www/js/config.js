d1ba1af464ee846934132b5c6593de87b58ba973
jhonny <steven.alvarez.v@gmail.com>
2013-11-28 13:54:45 -0400

    config.js

--- www/js/config.js
+++ www/js/config.js
@@ -1,9 +1,9 @@
 /************************************ VARIABLES DE CONFIGURACION *******************************************************/
 
 /************************************ server *******************************************************/
-var BASE_URL_APP = "https://www.patrocinalos.com/";
+var BASE_URL_APP = "https://www.patrocinalos.com/app/webroot/redsocialtest/";
 
 /************************************ localhost *******************************************************/
 //var BASE_URL_APP = "http://localhost/patrocinalos/";
-// var BASE_URL_APP = "http://10.0.0.100/patrocinalos/";
+//var BASE_URL_APP = "http://10.0.0.100/patrocinalos/";
 //var BASE_URL_APP = "http://192.168.0.1/patrocinalos/";
